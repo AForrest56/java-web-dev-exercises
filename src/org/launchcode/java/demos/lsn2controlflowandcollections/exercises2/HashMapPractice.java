f7a35a802851437297bf4a30c588dcd9b5776fda
Ashley <aforrest56@gmail.com>
2022-08-28 14:12:54 -0500

    HashMapPractice

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/exercises2/HashMapPractice.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/exercises2/HashMapPractice.java
@@ -2,6 +2,7 @@ package org.launchcode.java.demos.lsn2controlflowandcollections.exercises2;
 
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -12,10 +13,28 @@ public class HashMapPractice {
         Scanner input = new Scanner(System.in);
         String newStudent;
 
+        System.out.println("Enter student name: (or ENTER to finish)");
+
         do {
             System.out.println("Student: ");
             newStudent = input.nextLine();
+
+            if(!newStudent.equals("")) {
+                System.out.println("ID: ");
+                Integer newId = input.nextInt();
+                classRoster.put(newId, newStudent);
+
+                input.nextLine();
+            }
+
         } while(!newStudent.equals(""));
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
+            System.out.println(student.getValue() + " Id: " + student.getKey());
+        }
+
     }
 
 }
