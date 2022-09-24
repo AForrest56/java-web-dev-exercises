803aceecd99c7b68f5479ce55bb618e7c38b873f
Ashley <aforrest56@gmail.com>
2022-09-24 08:52:54 -0500

    Exercises Done

--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class FlavorComparator implements Comparator<Flavor> {
     @Override
     public int compare(Flavor flavor01, Flavor flavor02) {
+
         return flavor01.getName().compareTo(flavor02.getName());
     }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -12,17 +12,29 @@ public class Main {
         Comparator comparator2 = new ConeComparator();
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-        flavors.sort(comparator);
-        for (int i = 0; i < flavors.size(); i++){
-            System.out.println(flavors.get(i));
-        }
+//        flavors.sort(comparator);
+//        for (int i = 0; i < flavors.size(); i++){
+//            System.out.println(flavors.get(i));
+//        }
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+//        cones.sort(comparator2);
+//        for(int i = 0; i < cones.size(); i++) {
+//            System.out.println(cones.get(i));
+//        }
+
+        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        flavors.sort(comparator);
+        for (int i = 0; i < flavors.size(); i++){
+            System.out.println(flavors.get(i).getName());
+        }
+            System.out.println("\n*****\n");
+
         cones.sort(comparator2);
         for(int i = 0; i < cones.size(); i++){
-            System.out.println(cones.get(i));
+            System.out.println(cones.get(i).getName() + ": " + cones.get(i).getCost());
         }
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
     }
 }
